8177fd66d1b8e9412e8787af2986b07b8c1dc4f7
Manish Agrawal <32239632+magrawal23@users.noreply.github.com>
2021-06-04 20:32:02 -0400

    Updated project to .NET5.0. Included StackOverflow references to changes.

--- MVC_Start/Startup.cs
+++ MVC_Start/Startup.cs
@@ -6,55 +6,60 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+// https://stackoverflow.com/a/58072137
+using Microsoft.Extensions.Hosting;
 
 namespace MVC_Start
 {
-  public class Startup
-  {
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-    public void ConfigureServices(IServiceCollection services)
+    public class Startup
     {
-      // Added from MVC template
-      services.AddMvc();
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Added from MVC template
+            //services.AddMvc();
+
+            // https://stackoverflow.com/a/58772555/1385857
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // the version below came with the empty template
+        //public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        //{
+        //  if (env.IsDevelopment())
+        //  {
+        //    app.UseDeveloperExceptionPage();
+        //  }
+
+        //  app.Run(async (context) =>
+        //  {
+        //    await context.Response.WriteAsync("Hello World!");
+        //  });
+        //}
+
+        // this is the version from the MVC template. Compare to above for changes
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                //app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+              name: "default",
+              template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
     }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    // the version below came with the empty template
-    //public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    //{
-    //  if (env.IsDevelopment())
-    //  {
-    //    app.UseDeveloperExceptionPage();
-    //  }
-
-    //  app.Run(async (context) =>
-    //  {
-    //    await context.Response.WriteAsync("Hello World!");
-    //  });
-    //}
-
-    // this is the version from the MVC template. Compare to above for changes
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseBrowserLink();
-        app.UseDeveloperExceptionPage();
-      }
-      else
-      {
-        app.UseExceptionHandler("/Home/Error");
-      }
-
-      app.UseStaticFiles();
-
-      app.UseMvc(routes =>
-      {
-        routes.MapRoute(
-            name: "default",
-            template: "{controller=Home}/{action=Index}/{id?}");
-      });
-    }
-  }
 }
